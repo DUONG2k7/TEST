82bf069546bea594c3a94b2ca8de80bc62b73808
Im-Hung <vunguyenhung586@gmail.com>
2025-04-10 14:17:53 +0700

    Fix dgv va hien thi

--- DAL_QL/DbConnect.cs
+++ DAL_QL/DbConnect.cs
@@ -9,6 +9,12 @@ namespace DAL_QL
 {
     public class DbConnect
     {
+<<<<<<< HEAD
         protected string ConnectionString = @"Data Source=192.168.1.3,1433;Initial Catalog=PRO231;Persist Security Info=True;User ID=sa;Password=123;Encrypt=False";
+=======
+        protected string ConnectionString = @"Data Source=Hung-Vu\SQLEXPRESS;Initial Catalog=PRO231;Integrated Security=True";
+        //protected string ConnectionString = @"Data Source=192.168.1.5,1433;Initial Catalog=PRO231;Persist Security Info=True;User ID=sa;Password=123;Encrypt=False";
+>>>>>>> cdb6fc8 (Fix dgv)
     }
 }
+
